eb9d3606f711f1440e410274382e624a2a1c2cd5
sarareid <sara063@gmail.com>
2021-01-02 20:03:08 +0000

    First commit: styles changed and product description page added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+//   styleUrls: ['./app.component.css']
+  styles: ['p { font-weight: normal; font-size: 16px; font-family: Helvetica, Arial; img: 100% }']
 })
 export class AppComponent {
   title = 'app works!';
