273c133a59da25e42b27f7f16615082001a1eedb
sarareid <sara063@gmail.com>
2021-01-02 20:12:40 +0000

    styles updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-//   styleUrls: ['./app.component.css']
-  styles: ['p { font-weight: normal; font-size: 16px; font-family: Helvetica, Arial; img: 100% }']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'app works!';
