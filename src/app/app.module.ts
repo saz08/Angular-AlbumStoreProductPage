bae58cb4e475136f4cec29c4fc827f038070b601
sarareid <sara063@gmail.com>
2021-01-03 20:28:07 +0000

    Step 7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,11 @@ import { ProductTracklistingComponent } from './product-tracklisting/product-tra
 import { ProductListComponent } from './product-list/product-list.component';
 import {RouterModule, Routes} from '@angular/router';
 
-  appRoutes[]: Routes;
+const appRoutes: Routes = [
+  { path: 'products', component: ProductListComponent },
+  { path: 'product/:id', component: ProductPageComponent },
+  { path: '', redirectTo: 'products', pathMatch: 'full' }
+]
 
 @NgModule({
   declarations: [
@@ -24,7 +28,8 @@ import {RouterModule, Routes} from '@angular/router';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
