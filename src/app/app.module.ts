21eea5f25598c8a1b583fa53780674baa68d1d66
sarareid <sara063@gmail.com>
2021-01-03 20:24:11 +0000

    Step 7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,9 @@ import { ProductDescriptionComponent } from './product-description/product-descr
 import {ProductService} from './product.service';
 import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import {RouterModule, Routes} from '@angular/router';
+
+  appRoutes[]: Routes;
 
 @NgModule({
   declarations: [
